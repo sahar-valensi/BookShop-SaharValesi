9e08b9e3a6fa6d1d16a4052c4802182ae15e254f
sahar-valensi <saharva3@gmail.com>
2025-06-30 18:40:20 +0300

    add localStorage + search filters +msg modal

--- js/book.controller.js
+++ js/book.controller.js
@@ -26,14 +26,18 @@ function renderBooks() {
 
 function onRemoveBook(bookId) {
   removeBook(bookId);
+  showUserMsg('Book removed!');
   renderBooks();
 }
+
 function onUpdateBook(bookId) {
   const newPrice = +prompt("Enter a new price:");
   if (!newPrice || newPrice <= 0) return alert("Invalid Price");
   updateBookPrice(bookId, newPrice);
+  showUserMsg('Book price updated!');
   renderBooks();
 }
+
 function onShowDetails(bookId) {
   const book = getBookById(bookId);
   const elModal = document.querySelector(".book-details-modal");
@@ -43,19 +47,52 @@ function onShowDetails(bookId) {
      Price: ${book.price} `;
 
   elModal.showModal();
-
   //   renderBooks();
 }
 
 function onAddBook() {
   const title = prompt("Give me the name of the book:");
-  const price = +prompt("Give me the price of the book:");
+  const priceStr = prompt("Give me the price of the book:");
+
+  if (!title || !title.trim()) {
+    alert("Book title cannot be empty.");
+    return;
+  }
+
+  const price = +priceStr;
+  if (!priceStr || isNaN(price) || price <= 0) {
+    alert("Invalid price.");
+    return;
+  }
+
   const book = {
     id: makeId(),
-    title,
+    title: title.trim(),
     price,
   };
+
   addBook(book);
-//   console.log(gBooks)
- renderBooks()
+  showUserMsg('Book added!');
+  renderBooks();
+}
+
+function onSetFilterBy(filterValue) {
+  gFilterBy = filterValue;
+  renderBooks();
+}
+
+function onClearFilter() {
+  gFilterBy = '';
+  document.querySelector('.filter-wrapper input').value = '';
+  renderBooks();
+}
+
+function showUserMsg(msg) {
+  const elMsg = document.querySelector('.user-msg');
+  elMsg.innerText = msg;
+  elMsg.classList.remove('hidden');
+
+  setTimeout(() => {
+    elMsg.classList.add('hidden');
+  }, 2000);
 }
